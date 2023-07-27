933ea6eda78c5d8d982d519f9a6a91c8ec68f7fa
Tksi <24800668+Tksi@users.noreply.github.com>
2023-07-27 21:37:26 +0900

    feat: add favicon

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,5 +2,12 @@
 	console.log(1);
 </script>
 
+<svelte:head>
+	<title>Piano</title>
+	<meta name="robots" content="noindex nofollow" />
+</svelte:head>
+
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p>
+	Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+</p>
